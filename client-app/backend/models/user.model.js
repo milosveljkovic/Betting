0619b16b08c8626c0fc28211d0143f02f1381c76
Milos Veljkovic <veljkovicmilos559@gmail.com>
2020-02-05 17:55:05 +0100

    User routes and model

--- client-app/backend/models/user.model.js
+++ client-app/backend/models/user.model.js
@@ -4,7 +4,30 @@ const Schema= mongoose_.Schema;
 const UserSchema=new Schema({
     username:{
         type:String,
-        minlength:3
+        minlength:6,
+        required: true
+    },
+    password:{
+        type:String,
+        minlength:6,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    age:{
+        type:Number,
+        require:true,
+        min:18
+    },
+    credit:{
+        type: mongoose_.Decimal128,
+        min:0
+    },
+    is_admin:{
+        type:Boolean,
+        required:true
     }
 })
 
--- client-app/backend/routes/user.js
+++ client-app/backend/routes/user.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 let User_ = require('../models/user.model');
 
-//@PATH /user
-//@METHOD GET
+//@PATH     /user
+//@METHOD   GET
+//@DESC     find user by _id
 router.route('/').get((req,res)=>{
-    User_.find()
+    User_.findById(req.body._id)
     .then(users=>res.json(users))
-    .catch(err=>res.status(400).json('Error'+err))
+    .catch(err=>res.status(400).json('User not found: Error'+err))
 })
 
-//@PATH /user/add
-//@METHOD POST
+//@PATH     /user/add
+//@METHOD   POST
+//@DESC     add new user
 router.route('/add').post((req,res)=>{
-    const username=req.body.username;
-    const newUser = new User_({username})
-
+    const newUser = new User_(req.body);
     newUser.save()
-    .then(()=>res.json(newUser))
-    .catch(err=>res.status(400).json('Error'+err))
+    .then(()=>res.json(200).json('You are successfully registerd.'))
+    .catch(err=>res.status(400).json(err))
 })
 
 module.exports=router;
